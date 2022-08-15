e1146f659467e8e51a3067455713191727d42878
slorimerjr <slorimerjr@gmail.com>
2022-08-15 13:54:04 -0500

    Finished OOP coffee machine

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ while is_on:
         is_on = False
     elif choice == "report":
         print(coffee_maker.report())
+        print(money_machine.report())
     else:
        drink = menu.find_drink(order_name=choice)
        """need to verify that line 20 properly checks resources"""
        if coffee_maker.is_resource_sufficient(drink):
-           
+           if money_machine.make_payment(drink.cost):
+               coffee_maker.make_coffee(drink)
 
 
