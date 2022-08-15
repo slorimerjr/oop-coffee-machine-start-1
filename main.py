776c29df425facd47d2373d3bb3940313dbaa2a5
slorimerjr <slorimerjr@gmail.com>
2022-08-15 14:11:25 -0500

    Finished OOP coffee machine w/ teacher's solution

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
+#
 coffee_maker = CoffeeMaker()
 menu = Menu()
 money_machine = MoneyMachine()
 
+# is_on = True
+#
+# while is_on:
+#     choice = input(f"What would you like? {menu.get_items()}: ").lower()
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#     else:
+#        drink = menu.find_drink(order_name=choice)
+#        """need to verify that line 20 properly checks resources"""
+#        if coffee_maker.is_resource_sufficient(drink):
+#            if money_machine.make_payment(drink.cost):
+#                coffee_maker.make_coffee(drink)
+
+"""teacher's solution"""
+
 is_on = True
 
 while is_on:
-    choice = input(f"What would you like? {menu.get_items()}: ").lower()
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
     if choice == "off":
         is_on = False
     elif choice == "report":
-        print(coffee_maker.report())
-        print(money_machine.report())
+        coffee_maker.report()
+        money_machine.report()
     else:
-       drink = menu.find_drink(order_name=choice)
-       """need to verify that line 20 properly checks resources"""
-       if coffee_maker.is_resource_sufficient(drink):
-           if money_machine.make_payment(drink.cost):
-               coffee_maker.make_coffee(drink)
-
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
 
